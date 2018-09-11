66bc6a8d7d6327000af92df00ce41e8f4069e87b
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-09-11 08:13:39 -0400

    finished

---
+++ src/MathTester.java
@@ -0,0 +1,48 @@
+public class MathTester {
+
+    public static void main (String[]  args){
+
+
+ String firstWord;
+ String secondWord;
+ int firstNumber,secondNumber,total;
+
+
+
+
+
+
+
+
+ firstWord="dog";
+ secondWord="house";
+firstNumber=5;
+secondNumber=10;
+
+
+
+
+
+
+
+
+
+ System.out.println(firstWord+secondWord);
+ System.out.println(firstWord+(firstNumber+secondNumber));
+ System.out.println("firstNumber+secondNumber is "+(firstNumber+secondNumber));
+ System.out.println("firstNumber - secondNumber is "+(firstNumber - secondNumber));
+ System.out.println("secondNumber / firstNumber is "+(secondNumber/ firstNumber));
+ System.out.println("secondNumber * firstNumber is "+(secondNumber * firstNumber));
+ System.out.println("the modulus of the secondNumber by the firstNumber is "+(secondNumber %firstNumber));
+
+
+
+
+
+
+
+
+    }
+
+}
+
